00cf0a1c490a1d85cf10a1807131277ac398d74d
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 10:20:58 +0000

    Sat Oct 12 10:20:58 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,14 +1,23 @@
 package befaster.solutions.SUM;
 
-import befaster.runner.SolutionNotImplementedException;
-
 @SuppressWarnings("unused")
 public class SumSolution {
 
-    public int compute(int x, int y) {
-        if (x < 0 || y < 0 || x > 100 || y > 100)
-            throw new IllegalArgumentException();
-    }
+  /**
+   * Returns sums of two integers, each of the in [0, 100].
+   * Extension points: custom exceptions.
+   *
+   * @param x
+   * @param y
+   * @return x + y
+   */
+  public int compute(int x, int y) {
+    if (x < 0 || y < 0 || x > 100 || y > 100)
+      throw new IllegalArgumentException();
+
+    return x + y;
+  }
 
 }
+
 
