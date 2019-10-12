227884379e827d9f5a1b7e2ffb012ee73e5afe97
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 10:29:29 +0000

    Sat Oct 12 10:29:29 UTC 2019

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,5 +1,17 @@
+package befaster.solutions.HLO;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class HelloSolutionTest {
+  private HelloSolution helloSolution = new HelloSolution();
 
+  @Test
+  public void shouldReturnHelloDespiteTheInput() {
+    assertThat(helloSolution.hello(null), is("hello"));
+    assertThat(helloSolution.hello(""), is("hello"));
+  }
 }
+
