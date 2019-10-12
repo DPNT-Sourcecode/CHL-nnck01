5aba4018792206e6866223e95d973ead1144e927
sbx_user1051 <sbx_user1051@169.254.185.45>
2019-10-12 10:35:10 +0000

    Sat Oct 12 10:35:10 UTC 2019

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -9,13 +9,22 @@ public class HelloSolutionTest {
   private HelloSolution helloSolution = new HelloSolution();
 
   @Test
+  public void shouldReturnHelloWorldToEmpty() {
+    assertThat(helloSolution.hello(""), is("Hello, !"));
+  }
+  @Test
+  public void shouldReturnHelloWorldToNull() {
+    assertThat(helloSolution.hello(null), is("Hello, !"));
+  }
+  @Test
   public void shouldReturnHelloWorldToCraftsman() {
-    assertThat(helloSolution.hello("Craftsman"), is("Hello, World!"));
+    assertThat(helloSolution.hello("Craftsman"), is("Hello, Craftsman!"));
   }
   @Test
   public void shouldReturnHelloWorldToMtX() {
-    assertThat(helloSolution.hello("Mr. X"), is("Hello, World!"));
+    assertThat(helloSolution.hello("Mr. X"), is("Hello, Mr. X!"));
   }
 }
+
 
 
